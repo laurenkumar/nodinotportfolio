cc6abbc2039dd10a694cc2c0e61d4b4fcbb72107
dorian <lauren_d@outlook.fr>
2020-04-13 15:21:50 +0200

    first commit

--- src/const/WORKS.js
+++ src/const/WORKS.js
@@ -3,8 +3,7 @@ export default [
     key: 'sketch-threejs',
     title: 'Articles Rap',
     shortTitle: 'Articles Rap',
-    description:
-      'Interactive sketches made with three.js. I start to create the sketches from 2015 and continue even now.',
+    description: 'Interactive sketches made with three.js. I start to create the sketches from 2015 and continue even now.',
     credit: 'Auteur : Axel Nodinot',
     href: 'https://ykob.github.io/sketch-threejs/',
     textWidth: 1260
@@ -13,10 +12,8 @@ export default [
     key: 'warpdrive',
     title: 'Article Société',
     shortTitle: 'Article Société',
-    description:
-      "The Chrome Extension, a data visualization of user's activity on a chrome browser. I was responsible for a content named REQUEST VIEWER and devised how to mix 2D and 3D user interfaces.",
-    credit:
-      'Auteur : Axel Nodinot',
+    description: "The Chrome Extension, a data visualization of user's activity on a chrome browser. I was responsible for a content named REQUEST VIEWER and devised how to mix 2D and 3D user interfaces.",
+    credit: 'Auteur : Axel Nodinot',
     href: 'https://warpdrive-project.jp/',
     textWidth: 700
   }
